25101cf94394995abe5d74605ff999814f994309
blackhorsewu <victor.wh.wu@gmail.com>
2021-02-22 17:44:45 +0800

    Taken away Volume and only leave Profile

--- src/keyence_experimental/src/concave_scanner.cpp
+++ src/keyence_experimental/src/concave_scanner.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <ros/ros.h>
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
@@ -35,20 +36,54 @@ const static double KEYENCE_INFINITE_DISTANCE_VALUE_SI2 = -999.9970 / 1e3;
 
 ros::Publisher pub; // it will be initialised in main
 
-bool write_Y_file = true; // write the whole scan brief data to a file
+bool write_Y_file = false; // write the whole scan brief data to a file
 bool write_X_file = true; // write data of each scan line to a file
+
+double first_y = 0.0;
+double lastY = 0.0;
+double y = 0.0;
+double scanned_length = 0.0;
+double volume = 0.0;
 
 ofstream Yfile;
 ofstream Xfile;
 
 int line_no = 0;
 int file_no = 0;
-  
-int valid_begin, valid_end;
-
-int maxDoubleDotj;
 
 pcl::PointCloud<pcl::PointXYZ> pcl_Y_cloud; // cumulated cloud
+
+// Given a, b, find the length between them
+double length(geometry_msgs::Point a, geometry_msgs::Point b)
+{
+  return(sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2) + pow((b.z - a.z), 2)));
+}
+
+// Given a, b, both are vector of 3 doubles, return the DOT product.
+double dot(geometry_msgs::Point a, geometry_msgs::Point b)
+{
+  return(a.x*b.x + a.y*b.y + a.z*b.z);
+}
+
+// Given a, b, and p, find the projection of p on line ab.
+geometry_msgs::Point proj(geometry_msgs::Point a, geometry_msgs::Point b, geometry_msgs::Point p)
+{
+  geometry_msgs::Point ap, ab;
+  geometry_msgs::Point result;
+  double ratio;
+  
+  ap.x = p.x-a.x;  ab.x = b.x-a.x;
+  ap.y = p.y-a.y;  ab.y = b.y-a.y;
+  ap.z = p.z-a.z;  ab.z = b.z-a.z;
+  
+  ratio = dot(ap, ab)/dot(ab, ab);
+
+  result.x = a.x + ratio*ab.x;
+  result.y = a.y + ratio*ab.y;
+  result.z = a.z + ratio*ab.z;
+
+  return result;
+}
 
 // Given a mon return the name of the month.
 char *month_text(int mon)
@@ -123,7 +158,7 @@ void OpenSurfaceFile()
   ROS_INFO_STREAM("File: " << name_text << " will be created.");
   Yfile.open(name_text);
   ROS_INFO_STREAM("File: " << name_text << " was created.");
-  Yfile << "Width, Slice , Area, Plate , 1st Y, Y, Scanned Length, Volume\n";
+  Yfile << "Width, Slice , Area, 1st Y, Y, Scanned Length, Volume\n";
 }
 
 void OpenLineFile()
@@ -162,11 +197,11 @@ void OpenLineFile()
     ROS_INFO_STREAM("Folder: " << name_text << " was created.");
   }
   sprintf(min_text, "%d-profile_%d.csv", ltm->tm_min, file_no);
-  ROS_INFO_STREAM("Name text: " << name_text);
+  // ROS_INFO_STREAM("Name text: " << name_text);
   strcat(name_text, min_text);
-  ROS_INFO_STREAM("File: " << name_text << " will be created.");
+  // ROS_INFO_STREAM("File: " << name_text << " will be created.");
   Xfile.open(name_text);
-  ROS_INFO_STREAM("File: " << name_text << " was created.");
+  // ROS_INFO_STREAM("File: " << name_text << " was created.");
   Xfile << "J, Z, Average Z, D Avg Z, Avg D Avg Z, D Avg D Avg Z, Avg DD Avg Z\n";
 }
 
@@ -175,8 +210,14 @@ class Line_cloud  // The class
   private:
     // private attribute
     pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
-    int minDoubleDot1Z, minDoubleDot2Z;
+    double minDoubleDot1Z = 0.0;
+    double minDoubleDot2Z = 0.0;
     double maxDoubleDotZ = 0.0;
+
+    int minDoubleDot1i, minDoubleDot2i;
+    int valid_begin, valid_end = 0;
+
+    int maxDoubleDotj;
 
     std::string world_frame = "world";
 
@@ -194,7 +235,7 @@ class Line_cloud  // The class
         listener.waitForTransform(world_frame,
                                   ros_cloud->header.frame_id,
                                   ros::Time::now(),
-                                  ros::Duration(0.25));
+                                  ros::Duration(0.3));
         listener.lookupTransform (world_frame,
                                   ros_cloud->header.frame_id,
                                   ros::Time(0),
@@ -205,6 +246,8 @@ class Line_cloud  // The class
         ROS_ERROR("%s",ex.what());
       }
 
+      y = stransform.getOrigin().y();
+
       sensor_msgs::PointCloud2 transformed_ros_cloud;
       pcl_ros::transformPointCloud("world",
                                   stransform,
@@ -212,9 +255,19 @@ class Line_cloud  // The class
                                   transformed_ros_cloud);
       pcl::fromROSMsg(transformed_ros_cloud, pcl_cloud);
       pcl_Y_cloud.header.frame_id = pcl_cloud.header.frame_id; // cannot be done away with; must keep
+      // cumulate the line cloud into a profile cloud
       pcl_Y_cloud += pcl_cloud;
+      // Pass through the line cloud 3 times
+      // 1. work out the averaged differences between points
       average_differences();
+      // 2. find the width of the Groove
+      // width();
+      // 3. work out the cross-section area of the groove
+      // area();
+      line_no++;
+      lastY = y;
     }
+
     // Getter
     pcl::PointCloud<pcl::PointXYZ> get_pcl_cloud()
     {
@@ -247,7 +300,8 @@ class Line_cloud  // The class
         OpenLineFile();
       }
 
-      for (int i = 0; i < (cloudSize - 50); ++i)
+      // for (int i = 0; i < (cloudSize - 50); ++i)
+      for (int i = 0; i < cloudSize; ++i)
       {
         zz = pcl_cloud[i].z;
         if ((zz != KEYENCE_INFINITE_DISTANCE_VALUE_SI) && (zz != KEYENCE_INFINITE_DISTANCE_VALUE_SI2)
@@ -374,11 +428,13 @@ class Line_cloud  // The class
               davgdavgz[(j-m)-m] = avgdavgz[(j-m)-m] - avgdavgz[((j-m)-m)-1];
               sumdavgdavgz += davgdavgz[(j-m)-m];
               avgddavgz[((j-m)-m)-m] = sumdavgdavgz / w;
+              /*
               if (avgddavgz[((j-m)-m)-m] > maxDoubleDotZ)
               {
                 maxDoubleDotZ = avgddavgz[((j-m)-m)-m];
                 maxDoubleDotj = ((j-m)-m)-m;
               }
+              */
               if (write_X_file)
               {
                 Xfile << (((j-m)-m)-m) << ", " << Z[((j-m)-m)-m] << ", " << avgz[((j-m)-m)-m] << ", " << davgz[((j-m)-m)-m] << ", "  << avgdavgz[((j-m)-m)-m] << 
@@ -391,7 +447,9 @@ class Line_cloud  // The class
             j++;
           }
         }
-        else if (i >= (cloudSize - 50))
+        if ((i >= (cloudSize - 50)) && (valid_end == 0) && 
+            ((zz == KEYENCE_INFINITE_DISTANCE_VALUE_SI) || (zz == KEYENCE_INFINITE_DISTANCE_VALUE_SI2)
+              || (zz == std::numeric_limits<double>::infinity())))
         {
           valid_end = i;
         }
@@ -401,21 +459,22 @@ class Line_cloud  // The class
         Xfile.close();
         file_no++;
       }
+      // ROS_INFO_STREAM("Valid at: " << valid_begin << "; 1st y: " << first_y * 1e3);
+      // ROS_INFO_STREAM("Max DD position: " << maxDoubleDotj << " Line no.: " << line_no);
     }
 
-    double width(pcl::PointCloud<pcl::PointXYZ> pointcloud)
+    void width()
     {
       bool first = true;
       bool second = false;
       bool third = false;
 
-      int minDoubleDot1i, minDoubleDot2i;
-
       double zz;
+      double width;
 
       for (int i = valid_begin; i < valid_end; ++i)
       { // Second Loop through the Scan Line
-        zz = pointcloud[i].z;
+        zz = pcl_cloud[i].z;
 
         if ((zz != KEYENCE_INFINITE_DISTANCE_VALUE_SI) && (zz != KEYENCE_INFINITE_DISTANCE_VALUE_SI2)
               && (zz != std::numeric_limits<double>::infinity()))
@@ -451,19 +510,107 @@ class Line_cloud  // The class
       } // End of Second Loop through the Scan Line
     }
 
+    void area()
+    {
+      geometry_msgs::Point a, b, Top, Bot, Mid;
+      int begin = minDoubleDot1i + valid_begin;
+      int end = minDoubleDot2i + valid_begin;
+      double last_zB; // if inf then use last zB
+      double base; // in metre
+      double lastX, xIncrement;
+      double TopZ = 0.0;
+//      double BotZ = 0.0;
+      double thickness = 0.0;
+      double height = 0.0;
+      double lastHeight = height;
+      double area = 0.0;
+
+      a.x = pcl_cloud[begin].x;
+      a.y = pcl_cloud[begin].y;
+      a.z = pcl_cloud[begin].z;
+
+      b.x = pcl_cloud[end].x;
+      b.y = pcl_cloud[end].y;
+      b.z = pcl_cloud[end].z;
+
+      base = length(a, b);
+/*
+      Bot.x = pcl_cloud[maxDoubleDotj + valid_begin].x;
+      Bot.y = pcl_cloud[maxDoubleDotj + valid_begin].y;
+      Bot.z = pcl_cloud[maxDoubleDotj + valid_begin].z;
+
+      BotZ = Bot.z; */
+      TopZ = proj(a, b, Bot).z;
+      if (first_y == 0.0)
+      {
+        first_y = y;
+      }
+
+      if (line_no != 0)
+      {
+        thickness = (y - lastY) * cos(0.471233898038469); // Slice thickness is in Metre; it has slanted by 0.47 radian
+        scanned_length += thickness;
+      }
+
+      lastX = a.x;
+      for (int i = begin; i <= end; ++i)
+      { // Third Loop through the Scan Line
+        // Work out the cross-section area regardless if this is the first line
+        Bot.x = pcl_cloud[i].x;
+        Bot.y = pcl_cloud[i].y;
+        Bot.z = pcl_cloud[i].z;
+
+        xIncrement = abs(Bot.x - lastX);
+        lastX = Bot.x;
+
+        if ((Bot.z == KEYENCE_INFINITE_DISTANCE_VALUE_SI) || (Bot.z == KEYENCE_INFINITE_DISTANCE_VALUE_SI2)
+              || (Bot.z == std::numeric_limits<double>::infinity()))
+        { // if zB is inf then use last zB
+//          ROS_INFO("Bot.z is NaN!");
+          Bot.z = last_zB;
+        }
+        else // keep zB as last zB
+        {
+//          last_zB = Bot.z;
+
+          Top = proj(a, b, Bot); // The top point is the Bottom project onto the top line ab.
+          height = length(Top, Bot);
+
+          if (isnan(height))
+          {
+//            ROS_INFO("height is NaN!");
+            height = lastHeight;
+            exit;
+          }
+
+          area += height * xIncrement; // both height and x_increment are in Metre, therefore area in Metre square
+
+          if (((line_no != 0) && (height != 0.0) && (thickness != 0.0)) &&
+              ((Bot.z != KEYENCE_INFINITE_DISTANCE_VALUE_SI) && (Bot.z != KEYENCE_INFINITE_DISTANCE_VALUE_SI2)
+              && (Bot.z != std::numeric_limits<double>::infinity())))
+          {
+            /* Do not publish the Fillers */
+          }
+        }
+        lastHeight = height;
+        last_zB = Bot.z;
+      } // End of Third Loop through the Scan Line
+      area = area * cos(0.471233898038469);
+      volume += area * thickness;
+      // ROS_INFO_STREAM("Width: " << base * 1e3 << " mm");
+      // ROS_INFO_STREAM("Slice Thickness: " << thickness * 1e3 << " mm");
+      // ROS_INFO_STREAM("Scanned Length: " << scanned_length * 1e3 << " mm");
+      // ROS_INFO_STREAM("Y: " << y * 1e3);
+      // ROS_INFO_STREAM("TopZ: " << TopZ*1e3 << " BotZ: " << BotZ*1e3 << " Plate: " << (TopZ - BotZ) * 1e3);
+      // ROS_INFO_STREAM("Area: " << area * 1e6 << " mm2");
+      // ROS_INFO_STREAM("Volume: " << volume * 1e9 << " mm3\n");
+      if (write_Y_file)
+      {
+        Yfile << base * 1e3 << ", " << thickness * 1e3 << ", " << area * 1e6 << ", " << first_y * 1e3 << ", " 
+              << y * 1e3 << ", " << scanned_length * 1e3 << ", " << volume * 1e9 << "\n";
+      }
+    }
 };
-
-
-// Process a scan line.
-void process_line(pcl::PointCloud<pcl::PointXYZ> pointcloud)
-{
-  /* Pass through the line 3 times */
-  /* 1. work out the averaged differences between points */
-  // average_differences(pointcloud);
-  /* 2. find the width of the Groove */
-  // double groove_width = width(pointcloud);
-  /* 3. work out the cross-section area of the groove */
-}
 
 // This handles one scan line.
 void callback(const sensor_msgs::PointCloud2ConstPtr& ros_cloud)
@@ -471,9 +618,7 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& ros_cloud)
   // Create an object of Line_cloud (this will call the constructor)
   Line_cloud line_cloud(ros_cloud);
 
-  
   // The publisher is initialised in main
-
   pub.publish(pcl_Y_cloud);
 }
 
@@ -492,7 +637,21 @@ int main(int argc, char** argv)
   cloud_topic = "profiles"; // The cloud published by the Keyence Driver
 
   std::string topic = nh.resolveName(cloud_topic);
-  ROS_INFO_STREAM("Cloud service called; waiting for a PointCloud2 on topic " << topic);
+  ROS_INFO_STREAM("Cloud service called; waiting for a PointCloud2 on topic: " << topic);
+
+  // Visual tools
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("world");
+  visual_tools.deleteAllMarkers();
+
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().x() = 0.75;
+  text_pose.translation().z() = 0.45;
+  visual_tools.publishText(text_pose, "RoboWeld Groove Scanning", rvt::WHITE, rvt::XLARGE, false);
+  visual_tools.trigger();
+
+  char out_text[1500];
+  Eigen::Isometry3d out_text_pose = Eigen::Isometry3d::Identity();
 
   if (write_Y_file)
   {
